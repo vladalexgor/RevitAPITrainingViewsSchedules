e9911d685047cf740fc0535d05358ee15f884a0e
vladalexgor@ya.ru <vladalexgor@ya.ru>
2022-04-06 22:43:18 +0300

    Отработано занятие 7.9

--- MainViewViewModel.cs
+++ MainViewViewModel.cs
@@ -2,6 +2,7 @@
 using Autodesk.Revit.UI;
 using Prism.Commands;
 using RevitAPITrainingLibrary;
+using RevitAPITrainingViewsSchedules.Wrappers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,39 +16,82 @@ namespace RevitAPITrainingViewsSchedules
         private ExternalCommandData _commandData;
         private Document _doc;
 
-        public List<ViewPlan> Views { get; }
-        public List<ParameterFilterElement> Filters { get; }
+        public List<ScheduleWrapper> Schedules { get; }
         public DelegateCommand AddFilterCommand { get; }
+        public string ParameterName { get; set; }
+        public string ParameterValue { get; set; }
 
         public MainViewViewModel(ExternalCommandData commandData)
         {
             _commandData = commandData;
             _doc = _commandData.Application.ActiveUIDocument.Document;
-            Views = ViewsUtils.GetFloorPlanViews(_doc);
-            Filters = FilterUtils.GetFilters(_doc);
+            Schedules = ScheduleUtils.GetAllTheSchedules(_doc).Select(s => new ScheduleWrapper(s)).ToList();
             AddFilterCommand = new DelegateCommand(OnAddFilterCommand);
         }
 
-        public ParameterFilterElement SelectedFilter { get; set; }
-
         private void OnAddFilterCommand()
         {
-            if (SelectedViewPlan == null || SelectedFilter == null)
+            List<ScheduleWrapper> selectedSchedules = Schedules.Where(s => s.IsSelected).ToList();
+            if (selectedSchedules == null)
                 return;
-            using (var ts = new Transaction(_doc, "Set filter"))
+
+            using (var ts = new Transaction(_doc, "Add filter to schedule"))
             {
                 ts.Start();
-                SelectedViewPlan.AddFilter(SelectedFilter.Id);
-                OverrideGraphicSettings overrideGraphicSettings = SelectedViewPlan.GetFilterOverrides(SelectedFilter.Id);
-                overrideGraphicSettings.SetProjectionLineColor(new Color(255, 0, 0));
-                SelectedViewPlan.SetFilterOverrides(SelectedFilter.Id, overrideGraphicSettings);
-                ts.Commit();
-            }
+                foreach (var schedule in selectedSchedules)
+                {
+                    var scheduleDef = schedule.ViewSchedule.Definition;
+                    if (scheduleDef == null)
+                        continue;
 
-            RaiseCloseRequest();
+                    ScheduleField field = FindField(schedule.ViewSchedule, ParameterName);
+                    if (field == null)
+                    {
+                        SchedulableField schedulableField = FindSchedulableField(schedule.ViewSchedule, ParameterName);
+                        if (schedulableField == null)
+                            continue;
+
+                        field = scheduleDef.AddField(schedulableField);
+                    }
+
+                    if (field == null)
+                        continue;
+
+                    var filter = new ScheduleFilter(field.FieldId, ScheduleFilterType.Equal, ParameterValue);
+                    if (filter == null)
+                        continue;
+
+                    scheduleDef.AddFilter(filter);
+                }
+                ts.Commit();
+
+                RaiseCloseRequest();
+            }
         }
 
-        public ViewPlan SelectedViewPlan { get; set; }  
+        private SchedulableField FindSchedulableField(ViewSchedule viewSchedule, string parameterName)
+        {
+            var schedulableField = viewSchedule.Definition.GetSchedulableFields()
+                                        .FirstOrDefault(p => p.GetName(viewSchedule.Document) == parameterName);
+            return schedulableField;
+        }
+
+        private ScheduleField FindField(ViewSchedule viewSchedule, string parameterName)
+        {
+            ScheduleDefinition definition = viewSchedule.Definition;
+            var fieldCount = definition.GetFieldCount();
+
+            for (int i = 0; i < fieldCount; i++)
+            {
+                var field = definition.GetField(i);
+                var fieldName = field.GetName();
+
+                if (fieldName == parameterName)
+                    return definition.GetField(i);
+            }
+
+            return null;
+        }
 
         public event EventHandler CloseRequest;
         private void RaiseCloseRequest()
